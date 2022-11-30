2aadd73ecf27cbf94f2895f0f9ac0113a0a097b6
unknown <cepa1995cepa@gmail.com>
2022-11-30 15:50:20 +0100

    sesta vjezba

---
+++ vjezba06/spider.cpp
@@ -0,0 +1,22 @@
+#include "zivotinja.hpp"
+
+int spider::get_legs()
+{
+	return 0;
+}
+
+string spider::get_species()
+{
+	return name;
+}
+
+
+string tarantula :: get_species()
+{
+	return name; 
+}
+
+int tarantula :: get_legs() 
+{ 
+	return legs; 
+}
